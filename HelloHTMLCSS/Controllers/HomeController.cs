bcd353542a202d367bf56b30a55d3d3e14aa87ec
Alireza <Alireza@DESKTOP-TCNDLJL>
2023-02-07 16:54:06 +0330

    Add project files.

---
+++ HelloHTMLCSS/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using HelloHTMLCSS.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HelloHTMLCSS.Controllers
+{
+    public class HomeController : Controller
+    {      
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
